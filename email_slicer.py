e6ee14601d417f3584940951a5e4e4194e90e3bc
git-infs-info <59069667+git-infs-info@users.noreply.github.com>
2019-12-26 17:13:49 -0600

    Add files via upload

---
+++ email_slicer.py
@@ -0,0 +1,23 @@
+# get user email address
+email = input("What is your email address? ").strip()
+
+#slice out user name
+user = email[:email.index("@")]
+
+
+#slice domain name
+domain = email[email.index("@") + 1:]
+
+
+
+
+#format message
+output = "Your user name is {} and your domain is {}.".format(user, domain)
+
+
+
+#display output message
+print(output)
+
+
+
