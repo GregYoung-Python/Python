a1f2596747003bec0c0e3b708672c0605dd11cda
git-infs-info <59069667+git-infs-info@users.noreply.github.com>
2019-12-26 19:55:21 -0600

    Add files via upload

---
+++ booleans_comparison.py
@@ -0,0 +1,69 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> B = True
+>>> C = False
+>>> D = True
+>>> E = "True"
+>>> type("True")
+<class 'str'>
+>>> type(E)
+<class 'str'>
+>>> type(B)
+<class 'bool'>
+>>> 2 > 3
+False
+>>> 3 < 2
+False
+>>> 3 >2
+True
+>>> 2 < 3
+True
+>>> 100 > 99
+True
+>>> 199 < 200
+True
+>>> type(2 < 3)
+<class 'bool'>
+>>> 2 ==3
+False
+>>> 3==3
+True
+>>> 3 != 3
+False
+>>> 3 > 5
+False
+>>> 3 != 5
+True
+>>> 5 !=3
+True
+>>> 5 != 8
+True
+>>> 8 ==8
+True
+>>> 4 >= 3
+True
+>>> 4 >= 5
+False
+>>> 5 >= 5
+True
+>>> 10 <= 9
+False
+>>> 4 <= 2
+False
+>>> 99 <=100
+True
+>>> type(99 <=100)
+<class 'bool'>
+>>> 100 != 100
+False
+>>> 100 == 100
+True
+>>> 200 =< 201
+SyntaxError: invalid syntax
+>>> 200 <= 201
+True
+>>> 401 < 402
+True
+>>> 401 > 10
+True
+>>> 
