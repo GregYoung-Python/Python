6bb7a2e00f7175e90ce73f729cea031f47ed770c
git-infs-info <59069667+git-infs-info@users.noreply.github.com>
2019-12-28 18:02:58 -0600

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,53 @@
+num1 = 4500
+num2 = 5400
+if num2 < num1:
+    print("num2 is greater than num1.")
+
+num4 = 9900
+num5 = 10000
+num6 = 9900
+num7 = 100
+
+if num4 >= num6:
+    print("num4 is greater than or equal to num 6.")
+
+    if (num4 + num7) == num5:
+        print("num4 + num7 equals num5")
+
+if num5 <= num4:
+    print("num5 is less than or equal to num4")
+else:
+        print("num5 is larger than num4")
+
+num8 = 4912
+
+if (num5 - num7) <= num6:
+    print("num5 minus num7 is equal to or less than num6")
+
+    if num8 == 4912:
+        print("num8 is equal to 4912")
+
+        if num8 != 4912:
+            print("num8 is  not equal to 4912")
+        else:
+                print("num8 is equal to 4912")
+
+num9 = 8219
+num10 = 8220
+
+if num9 < num10:
+    print("num9 is smaller than num10")
+elif num9 > num10:
+    print("num9 is larger than num10")
+else:
+    print("print num9 is equal to num10")
+    
+    
+
+
+
+
+
+
+
+
