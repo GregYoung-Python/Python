4194cb82a4cc3416ac71c9e2e3d3db1f95ef4a1f
Greg Young <59069667+GregYoung-Python@users.noreply.github.com>
2019-12-31 17:58:34 -0600

    Add files via upload

---
+++ add_remove_in_list.py
@@ -0,0 +1,31 @@
+app_users = ["Alice", "Bob", "Cindy", "Bill", "Joey", "Susan", "Walter"]
+print(len(app_users))
+
+while True:
+    print("Hello my name is William! ")
+    name = input("What is your name?: ").strip().capitalize()
+
+    if name in app_users:
+        print("Hello {}! " .format(name))
+        remove = input("Would you like to be removed from the list? (y/n)?: ").strip().lower()
+
+        if remove == "y":
+            print(app_users)
+            app_users.remove(name)
+            print(app_users)
+        elif remove == "n":
+                print("Great we are glad you have decided to stay with us!")
+    
+    else:
+        print("Your name is not in the list yet {}".format(name))
+        add_name = input("Would you like to be added to the list (y/n)?: ").strip().lower()
+        if add_name == "y":
+            print(app_users)
+            app_users.append(name)
+            print(app_users)
+        elif add_name == "n":
+            print("No problem let us know if you change your mind!")
+            
+
+
+
