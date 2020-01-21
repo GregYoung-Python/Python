3b9f1d97967a20a94ccac65d067950437e6883e5
gregyoung2600 <56743328+gregyoung2600@users.noreply.github.com>
2020-01-21 02:30:33 -0600

    variable_scope.py

---
+++ variable_scope.py
@@ -0,0 +1,29 @@
+a = 100
+b = 2000
+# this is a global variable 
+def f1():
+    print(a)
+
+def f2():
+    print(a)
+
+
+f1()
+f2()
+
+# b is not a global variable here
+def f3():
+    # b = 200
+    b = a + 10
+    print(b)
+
+def f4():
+    b = 50
+    print(b)
+
+f3()
+f4()
+print(b)
+print(a)
+
+
